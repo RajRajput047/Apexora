643fbb7f65ff29feb6e58ef5703da216db59ba98
Raj Rajput <rajrajput124h@gmail.com>
2026-02-21 15:51:14 +0530

    folder path vchange

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webdev_services.settings')
+    # Updated from 'webdev_services.settings' to 'Apexora.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Apexora.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
