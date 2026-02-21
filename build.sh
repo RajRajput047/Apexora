c5daa521d6cc521184400e9e4b2fbb843567979c
Raj Rajput <rajrajput124h@gmail.com>
2026-02-22 01:44:59 +0530

    remove all filesrequirement update

--- build.sh
+++ build.sh
@@ -2,7 +2,20 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies
+# Install system dependencies for Pillow
+apt-get update && apt-get install -y \
+    libjpeg-dev \
+    zlib1g-dev \
+    libfreetype6-dev \
+    liblcms2-dev \
+    libwebp-dev \
+    tcl8.6-dev \
+    tk8.6-dev \
+    python3-tk \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files
