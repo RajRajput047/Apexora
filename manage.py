7689585d4dc57c7a42c61461a889dbb56d235dbb
Raj Rajput <rajrajput124h@gmail.com>
2026-02-22 02:30:42 +0530

    remove all filesrequirement update

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webdev_services.settings')
+    # This line should point to your correct settings module
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Apexora.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
