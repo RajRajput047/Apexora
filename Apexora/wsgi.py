7f5c30999e73fae1e22699c87941a9c2e3739464
Raj Rajput <rajrajput124h@gmail.com>
2026-02-21 16:23:06 +0530

    wsgi change

--- Apexora/wsgi.py
+++ Apexora/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 # Changed from 'webdev_services.settings' to 'Apexora.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Apexora.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'Apexora.settings'
+)
 
-application = get_asgi_application()
+application = get_wsgi_application()
