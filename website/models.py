460db9a5f38e47bf81a4a099317daa60f80f0cee
Raj Rajput <rajrajput124h@gmail.com>
2026-02-22 13:25:01 +0530

    Force Python 3.11 and fix build issues

--- website/models.py
+++ website/models.py
@@ -13,31 +13,3 @@ class ContactLead(models.Model):
     def __str__(self):
         return f"{self.name} - {self.email}"
 
-class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = CloudinaryField('image', folder='projects/')
-    url = models.URLField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.title
-    
-    class Meta:
-        ordering = ['-created_at']
-
-class TeamMember(models.Model):
-    name = models.CharField(max_length=100)
-    position = models.CharField(max_length=100)
-    bio = models.TextField()
-    photo = CloudinaryField('photo', folder='team/')
-    facebook = models.URLField(blank=True, null=True)
-    twitter = models.URLField(blank=True, null=True)
-    linkedin = models.URLField(blank=True, null=True)
-    order = models.IntegerField(default=0)
-    
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        ordering = ['order', 'name']
